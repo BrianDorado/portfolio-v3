cdda1c3667afb1f4a5035c0a439415f3655ab826
unknown <manateeman24@hotmail.com>
2019-07-04 23:18:12 -0600

    initial setup

---
+++ src/Pages/Static/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            Here is the Home App
+        </div>
+    )
+}
+
+export default Home
---
+++ src/Root.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Root.css';
+import Home from './Pages/Static/Home/'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      Here is my portfolio
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
