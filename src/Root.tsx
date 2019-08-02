9ea6778f4bf94b6cd66ecea4339174ba8584b42c
unknown <manateeman24@hotmail.com>
2019-08-02 12:12:56 -0600

    prettier

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import {Router, Route, Redirect, Switch} from 'react-router-dom'
-import history from './Config/history'
-import Home from './Pages/Static/Home/'
-import Page from './Pages/Static/404'
+import { Router, Route, Redirect, Switch } from 'react-router-dom';
+import history from './Config/history';
+import Home from './Pages/Static/Home/';
+import Page from './Pages/Static/404';
 import Kata from './Pages/Kata';
-import { routeNotFound, homeRoute, kataRoute, projectsRoute } from './Config/Navigation/routes';
+import {
+  routeNotFound,
+  homeRoute,
+  kataRoute,
+  projectsRoute,
+} from './Config/Navigation/routes';
 import TopNav from './Components/TopNav/TopNav';
 import Projects from './Pages/Projects/Projects';
 import './Root.scss';
 
 const App: React.FC = () => {
   return (
-    <Router history={history} >
-      <TopNav/>
-      <Switch>
-      <Route exact path={homeRoute} component ={Home} />
-      <Route exact path={kataRoute} component ={Kata} />
-      <Route exact path ={projectsRoute} component ={Projects}/>
-      <Route exact path={routeNotFound} component ={Page} />
-      <Redirect to={routeNotFound} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <Router history={history}>
+        <TopNav />
+        <Switch>
+          <Route exact path={homeRoute} component={Home} />
+          <Route exact path={kataRoute} component={Kata} />
+          <Route exact path={projectsRoute} component={Projects} />
+          <Route exact path={routeNotFound} component={Page} />
+          <Redirect to={routeNotFound} />
+        </Switch>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
