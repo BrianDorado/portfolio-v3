8af88423b3c661c4637d12e8c52fe64fa4336a2d
unknown <manateeman24@hotmail.com>
2019-09-05 20:54:44 -0600

    images to skills block

--- src/Components/skills/skills.tsx
+++ src/Components/skills/skills.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import JSLogo from '../../images/JavaScript-logo.png'
+import HTML from '../../images/1200px-HTML5_logo_and_wordmark.svg.png'
+import CSS from '../../images/CSS3_logo_and_wordmark.svg.png'
+import Node from '../../images/nodejs-logo.jpg'
+import GIT from '../../images/Git.png'
+import SASS from '../../images/2000px-Sass_Logo_Color.svg.png'
+import GraphQL from '../../images/GraphQL_Logo.svg.png'
 import {
   projectsRoute,
   aboutRoute,
@@ -11,7 +18,20 @@ import './TopNav.scss';
 
 const TopNav: React.FC = () => {
   return (
-  <div>Here will be images with links to relevant demonstrations of highlighted skills</div>
+  <div>
+      <h2>Brian Larson - Development Skills</h2>
+      Here will be images with links to relevant demonstrations of highlighted skills
+      {/* Should this be a list? */}
+      <div className="" >Box of Skills
+          <img src={JSLogo} alt="Javascript"/>
+          <img src={HTML} alt="HTML5"/>
+          <img src={CSS} alt="CSS"/>
+          <img src={Node} alt="Node"/>
+          <img src={GIT} alt="GIT"/>
+          <img src={SASS} alt="SASS / SCSS"/>
+          <img src={GraphQL} alt="Graph QL"/>
+      </div>
+  </div>
   );
 };
 
