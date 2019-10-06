c787a438832a2c52e134313a2e53eee4505cfccb
unknown <manateeman24@hotmail.com>
2019-10-06 00:58:25 -0600

    footer with sm icons

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import {Icon} from 'antd'
+import Twitter from '../../images/Black-twitter-logo.png'
+import LinkedIn from '../../images/Black-LinkedIn-Logo.png'
+import Github from '../../images/Github.svg'
+import './Footer.scss'
 
 const Contact: React.FC = () => {
   return (
-    <div>
-      Here is my twitter Here is my Github Here is my Gitlab Here is my Codepen
-      Here is LinkedIn Here is my email
-      <a href="https://www.linkedin.com/in/larsonbrianj/">LinkedIn</a>
-      <a href="https://github.com/BrianDorado">Github</a>
-    </div>
+    <footer className='home-footer'>
+      {/* <a href="https://www.linkedin.com/in/larsonbrianj/">
+        {' '}
+        <img src={LinkedIn} alt="LinkedIn" />{' '}
+      </a>
+      <a href="https://github.com/BrianDorado">
+        {' '}
+        <img src={Github} alt="Github" />
+      </a>
+      <a href="https://twitter.com/Pandamanatee">
+        <img src={Twitter} alt="Twitter" />
+      </a> */}
+      <Icon type="github" />
+      <Icon type="twitter" />
+      <Icon type="linkedin" />
+    </footer>
   );
 };
 
--- src/Pages/Static/Home/Home.tsx
+++ src/Pages/Static/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Layout, Icon } from 'antd';
+// import Footer from '../../../Components/Footer'
 import AWS from '../../../images/AWS.png'
 import JS from '../../../images/JavaScript-logo.png'
 import TS from '../../../images/ts.png'
@@ -10,79 +11,88 @@ import HTML from '../../../images/1200px-HTML5_logo_and_wordmark.svg.png'
 import CSS from '../../../images/CSS3_logo_and_wordmark.svg.png'
 import './Home.scss';
 
-// const {Header, Content, Footer, Sider} = Layout
+const {Header, Content, Footer} = Layout
 
 const Home: React.FC = () => {
   return (
     <Layout>
-      <section className="developer-synopsis-container">
-        <div>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?{' '}
-          </p>
+      <Content>
+        <section className="developer-synopsis-container">
+          <div>
+            <p>
+              I am  Full Stack developer working for a company called Partnet. I specialize in React, Javascript, Typescript and AWS. 
+              <strong>School:</strong> I was recently accepted into Utah Valley University and plan to start studying in the spring. 
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
+              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
+              velit, sed quia non numquam eius modi tempora incidunt ut labore
+              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
+              veniam, quis nostrum exercitationem ullam corporis suscipit
+              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+              vel eum iure reprehenderit qui in ea voluptate velit esse quam
+              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+              voluptas nulla pariatur?{' '}
+            </p>
+          </div>
+        </section>
+        <section>
+          <div className="developer-skills-images">
+            <ul>
+              <li>
+                <img src={ReactLogo} alt="React" />{' '}
+              </li>
+              <li>
+                <img src={AWS} alt="AWS" />{' '}
+              </li>
+              <li>
+                <img src={GraphQL} alt="GraphQL" />{' '}
+              </li>
+              <li>
+                <img src={Node} alt="Node" />{' '}
+              </li>
+              <li>
+                <img src={JS} alt="Javascript" />{' '}
+              </li>
+              <li>
+                <img src={TS} alt="Typescript" />{' '}
+              </li>
+              <li>
+                <img src={HTML} alt="HTML5" />{' '}
+              </li>
+              <li>
+                <img src={CSS} alt="CSS" />{' '}
+              </li>
+            </ul>
+          </div>
+        </section>
+        <div className="action-alert-userScroll">
+          <Icon type="down" className="action-alert-downArrow" />
         </div>
-      </section>
-      <section>
-        <div className="developer-skills-images">
-          <ul>
-            <li>
-              <img src={ReactLogo} alt="React" />{' '}
-            </li>
-            <li>
-              <img src={AWS} alt="AWS" />{' '}
-            </li>
-            <li>
-              <img src={GraphQL} alt="GraphQL" />{' '}
-            </li>
-            <li>
-              <img src={Node} alt="Node" />{' '}
-            </li>
-            <li>
-              <img src={JS} alt="Javascript" />{' '}
-            </li>
-            <li>
-              <img src={TS} alt="Typescript" />{' '}
-            </li>
-            <li>
-              <img src={HTML} alt="HTML5" />{' '}
-            </li>
-            <li>
-              <img src={CSS} alt="CSS" />{' '}
-            </li>
-          </ul>
-        </div>
-      </section>
-      <div className="action-alert-userScroll">
-        <Icon type="down" className="action-alert-downArrow" />
-      </div>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?{' '}
-      </p>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?{' '}
+        </p>
+      </Content>
+      <Footer style={{ display: 'flex', justifyContent: 'space-around' , alignItems: 'center'}}>
+        <Icon type="github" style={{ fontSize: '35px' }} />
+        <Icon type="twitter" style={{ fontSize: '35px' }} />
+        <Icon type="linkedin" style={{ fontSize: '35px' }} />
+      </Footer>
     </Layout>
   );
 };
