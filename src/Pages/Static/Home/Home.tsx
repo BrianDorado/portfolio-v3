1be2f232cc7495924343b711aa5e89b1cc389a19
unknown <manateeman24@hotmail.com>
2019-10-12 12:38:56 -0600

    changes made

--- src/Pages/Static/Home/Home.tsx
+++ src/Pages/Static/Home/Home.tsx
@@ -19,26 +19,20 @@ const Home: React.FC = () => {
       <Content>
         <section className="developer-synopsis-container">
           <div>
+            {/* Should go to line 41 */}
             <p>
-              I am Full Stack developer working for a company called Partnet. I
-              specialize in React, Javascript, Typescript and AWS.
-              <strong>School:</strong> I was recently accepted into Utah Valley
-              University and plan to start studying in the spring. Sed ut
-              perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-              velit, sed quia non numquam eius modi tempora incidunt ut labore
-              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-              veniam, quis nostrum exercitationem ullam corporis suscipit
-              laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
-              vel eum iure reprehenderit qui in ea voluptate velit esse quam
-              nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-              voluptas nulla pariatur?{' '}
+              I am Full Stack developer specializing in React, Javascript,
+              Typescript, AWS, Node and many other technologies. Currently I
+              work for a company called Partnet as a Full Stack Developer. Our
+              current contract is for the ICON (Integrated Casting Online
+              Network) portal.
             </p>
+              <strong>School:</strong>
+              <p>
+
+              I was recently accepted into Utah Valley
+              University and plan to start studying in the Spring, 2019.
+              </p>
           </div>
         </section>
         <section>
@@ -90,20 +84,17 @@ const Home: React.FC = () => {
           qui dolorem eum fugiat quo voluptas nulla pariatur?{' '}
         </p>
       </Content>
-      <Footer
-      className='home-page-footer'
-      >
-        <div className='social-media-container'>
-
-        <a href="https://github.com/BrianDorado">
-          <Icon type="github" className='social-media-icon' />
-        </a>
-        <a href="https://twitter.com/Pandamanatee">
-          <Icon type="twitter" className='social-media-icon' />
-        </a>
-        <a href="https://www.linkedin.com/in/larsonbrianj/">
-          <Icon type="linkedin" className='social-media-icon' />
-        </a>
+      <Footer className="home-page-footer">
+        <div className="social-media-container">
+          <a href="https://github.com/BrianDorado">
+            <Icon type="github" className="social-media-icon" />
+          </a>
+          <a href="https://twitter.com/Pandamanatee">
+            <Icon type="twitter" className="social-media-icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/larsonbrianj/">
+            <Icon type="linkedin" className="social-media-icon" />
+          </a>
         </div>
       </Footer>
     </Layout>
